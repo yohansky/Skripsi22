b94f643beae08e3379438fb39923b025d86a873c
Yohansky99 <69236028+yohansky@users.noreply.github.com>
2022-10-18 16:46:03 +0700

    Add files via upload

---
+++ src/demo_program/javaconnect.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo_program;
+/**
+ *
+ * @author User
+ */
+import java.sql.*;
+import javax.swing.*;
+public class javaconnect {
+    
+    Connection conn= null;
+    public static Connection ConnecrDb(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/protonema","root",""); 
+            JOptionPane.showMessageDialog(null, "Connection Established");        
+        return conn;
+            }catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e);
+                return null;
+            }
+    }
+    
+}
